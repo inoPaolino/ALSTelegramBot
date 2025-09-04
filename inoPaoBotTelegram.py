e6b78ad6351974e7b1316ecc18deac9ab12a4235
inoPaolino <disafrapa92@gmail.com>
2025-09-04 18:34:30 +0200

    Update inoPaoBotTelegram.py

--- inoPaoBotTelegram.py
+++ inoPaoBotTelegram.py
@@ -54,7 +54,7 @@ def main():
 
     job_queue = app.job_queue
     job_queue.run_repeating(keep_api_alive, interval=600, first=10)
-    threading.Thread(target=app.run_polling, daemon=True).start()
+    app.run_polling()
         
 def keep_alive():
     port = int(os.environ.get("PORT", 10000))
@@ -64,10 +64,10 @@ def keep_alive():
     httpd.serve_forever()
         
 if __name__ == "__main__":
+    import asyncio
+    # Avvia il keep_alive in background
+    threading.Thread(target=keep_alive, daemon=True).start()
     main()
-    # Avvia il server HTTP finto
-    keep_alive()
-
 
 
 
