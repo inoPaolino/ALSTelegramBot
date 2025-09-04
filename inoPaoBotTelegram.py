66e58adb94f1a38bb3ad99694d0b2acbbc1b7cdf
inoPaolino <disafrapa92@gmail.com>
2025-09-04 17:50:59 +0200

    Update inoPaoBotTelegram.py

--- inoPaoBotTelegram.py
+++ inoPaoBotTelegram.py
@@ -31,10 +31,17 @@ def get_predator_cap():
         
     except Exception as e:
         print(f"⚠️ Errore durante la richiesta: {e}")
+        
+async def error_handler(update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"❌ Update {update} ha generato l’errore {context.error}")
+
 
 async def pred(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(latest_pred_data)
-    
+    if update.message:
+        await update.message.reply_text(latest_pred_data)
+    elif update.effective_chat:  # fallback: manda il messaggio al chat_id
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=latest_pred_data)
+        
 async def keep_api_alive(context: ContextTypes.DEFAULT_TYPE):
     _ = get_predator_cap()
     # non succede nulla, serve solo per tenere il traffico attivo di dati
@@ -43,6 +50,8 @@ async def keep_api_alive(context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("pred", pred))
+    app.add_error_handler(error_handler)
+
     job_queue = app.job_queue
     job_queue.run_repeating(keep_api_alive, interval=600, first=10)
     
@@ -60,6 +69,7 @@ if __name__ == "__main__":
     threading.Thread(target=keep_alive, daemon=True).start()
     # Avvia il server HTTP finto
     main()
+
 
 
 
